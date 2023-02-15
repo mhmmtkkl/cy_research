71d4d769d03a35ba064da282af0608ee1b8cb8cc
Chae Woon Jung <cjung@chae-jung.lan>
2023-02-14 21:00:39 -0600

    webkit, script

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,6 @@
-const { defineConfig } = require('cypress');
+import { defineConfig } from 'cypress';
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    projectId: '82a1no',
-  },
+export default defineConfig({
+  experimentalWebKitSupport: true,
+  e2e: {},
 });
